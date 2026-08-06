e9a45cd6a095d9cc4e89e6232bbdef67340bcc6f
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-08-06 19:11:01 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.school_grading_app"
+    namespace = "com.example.secretariat_app"
     compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.school_grading_app"
+        applicationId = "com.example.secretariat_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
